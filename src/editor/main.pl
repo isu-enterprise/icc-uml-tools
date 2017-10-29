7d7c1b627d030b9d2c4b68f374b8b903e6288862
Evgeny Cherkashin <eugene@irnok.net>
2017-10-29 14:46:14 +0800

    Run the interface for the first time.

--- src/editor/main.pl
+++ src/editor/main.pl
@@ -1,4 +1,28 @@
 
 :- use_module('../uml.pl').
-:- test(X), write(X).
+:- use_module(library(plgi)).
+
+:- plgi_use_namespace('Gtk').
+
+on_button_pressed(_Button, _UserData) :-
+	writeln('Hello World!').
+
+on_delete_window(_Widget, _Event, _UserData, true) :-
+	gtk_main_quit.
+
+main :-
+	% predicate_property(user:main, file(File)),
+	% file_directory_name(File, Directory),
+	% format(atom(GladeFile), '~w/36-builder.glade', [Directory]),
+
+	gtk_builder_new(Builder),
+	gtk_builder_add_from_file(Builder, 'application_window.glade', _),
+	gtk_builder_connect_signals(Builder, {null}),
+
+	gtk_builder_get_object(Builder, 'application_window', Window),
+	gtk_widget_show_all(Window),
+	gtk_main,
+	halt.
+
+:- main.
 :- halt.
--- src/uml.pl
+++ src/uml.pl
@@ -1,9 +1,14 @@
 :- module(uml, [test_structure/1]).
 
 test_structure('People'('Class',[
+                  'Persistent'('Stereotype',[]),
                   name('Attribute', [
-                           public('VisibilityPublic',[]),
-                           'String'('PrimitiveType',[])
-                       ])
+                           public('VisibilityKind',[]),
+                                 'String'('PrimitiveType',[])
+                       ]),
+                  getName('Operation',[
+                              get('Property',[]),
+                              public('VisibilityKind', [])
+                          ])
               ])
     ).
